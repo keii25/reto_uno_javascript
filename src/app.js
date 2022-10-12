e7f2cf72a316326c10f7bc255191f3b6549482e3
keii25 <kevin.martinezc@hotmail.com>
2022-10-12 18:04:14 -0500

    update src/app.js

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
-var scanf = require('scanf')
+var scanf = require('scanf');
 
-console.log("Ingresar Edad: ")
-let age = scanf('%d')
+console.log("Ingresar Edad: ");
+let age = scanf('%d');
 
-isAdult = 18
+const ADULT = 18;
 
 if(isNaN(age))
-    console.log('Debe introducir un numero valido')
+    console.log('Debe introducir un numero valido');
 else
-    if (age >= isAdult)
-        console.log("Puede Conducir")
+    if (age >= ADULT)
+        console.log("Puede Conducir");
     else
-        console.log("No Puede Conducir")
+        console.log("No Puede Conducir");
 
 
 
